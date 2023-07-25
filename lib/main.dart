e7e083da4b07740db5f01d5f7e5b30fd125abd82
ZainMughal329 <zainmughal4379329@gmail.com>
2023-07-26 02:24:45 +0500

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(),
+      home: Scaffold(
+        body: SafeArea(child: Column(children: [Center(child: Text("here we go"),)],)),
+      ),
 
     );
   }
